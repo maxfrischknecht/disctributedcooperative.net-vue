17727f18968ab04d5d5abb9797b8f80760f0f971
Max Frischknecht <max@debutdebut.com>
2021-03-24 14:52:16 +0100

    add p5 random img movement

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -14,7 +14,39 @@
   export default {
     name: 'Map',
     mounted() {
-      Draggable.create(this.$refs.p1);
+      const map = this.$refs.p1;
+      Draggable.create(map);
+
+      const script = (p5) => {
+
+        let location = new p5.createVector(-3000, 3000);
+        let noff = new p5.createVector(p5.random(-3000, 3000),p5.random(-3000, 3000));
+        
+        // setInterval(() => { 
+        //   location.x = p5.map(p5.noise(noff.x),0,1,0,-3000, 3000);
+        //   location.y = p5.map(p5.noise(noff.y),0,1,0,-3000, 3000);
+          
+        //   noff.add(0.1,0.1,0);
+        //   console.log(location.x, location.y)
+
+        //   map.style.transform = "translate(" + location.x + "px ,"  + location.y + "px)";
+        // }, 3000)
+        // NOTE: Draw is here
+        p5.draw = () => {
+          // let x_noise = p5.noise(100) * 100;
+          // console.log(x_noise)
+
+          location.x = p5.map(p5.noise(noff.x),0,1,0,-3000, 3000);
+          location.y = p5.map(p5.noise(noff.y),0,1,0,-3000, 3000);
+          
+          noff.add(0.002 ,0.002 ,0);
+          // console.log(location.x, location.y)
+
+          map.style.transform = "translate(" + location.x + "px ,"  + location.y + "px)";
+        }
+      }
+      const P5 = require('p5');
+      new P5(script)
     }
   }
 </script>
